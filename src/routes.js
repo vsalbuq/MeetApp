5de72cae7692c9164e74f1de6f78da55cf558d88
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-06-27 17:26:36 -0300

    feat: add user authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 // TODO: Create user
 routes.post('/users', UserController.store);
+
 // TODO: Authenticate user
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+
 // TODO: Update user
 
 export default routes;

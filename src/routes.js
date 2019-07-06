ac6d0493a6d2a1b87f54ec7e4d76a718d0e8ead7
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2019-07-06 09:29:56 -0300

    feature: adds File migration, model, controller and route to the aplication.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // TODO: Create user
 routes.post('/users', UserController.store);
@@ -15,5 +21,7 @@ routes.use(authMiddleware);
 
 // TODO: Update user
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
